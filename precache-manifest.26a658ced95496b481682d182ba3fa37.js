361190cac304e1174b5d9675037365ab2c027539
Leelarosy <rosyleela@gmail.com>
2019-12-16 07:53:29 +0530

    initial deployment

---
+++ precache-manifest.26a658ced95496b481682d182ba3fa37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6603187937ab35fdee9f51347dc23f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b9d2994b82495b44b0a",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "ca83016230498bd87044",
+    "url": "/static/css/main.1f6638a6.chunk.css"
+  },
+  {
+    "revision": "3b9d2994b82495b44b0a",
+    "url": "/static/js/2.c0313ae3.chunk.js"
+  },
+  {
+    "revision": "ca83016230498bd87044",
+    "url": "/static/js/main.cc74897b.chunk.js"
+  },
+  {
+    "revision": "4155b18966dec7c855fe",
+    "url": "/static/js/runtime-main.aadf17d2.js"
+  }
+]);
